0fe6cb02864b9c038e682a01668f441f1d883c0e
PokaCris <after_the_rain999@mail.ru>
2025-10-13 21:11:19 +0300

    new commit

--- src/App.php
+++ src/App.php
@@ -5,5 +5,16 @@ namespace Zver\Quiz;
 class App {
         public function __construct() {}
 
-        public function run() {}      
+        public function run() {
+                $responce = [
+                        'question' => 'Какое дерево становится мокрым после дождя?',
+                        'answer' => [
+                                'Береза',
+                                'Елка',
+                                'Любое',
+                                'Дуб'
+                        ]
+                ];
+                echo json_encode($responce);
+        }      
 }
