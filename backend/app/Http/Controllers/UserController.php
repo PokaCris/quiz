2a7901180b852724cfc0171e1234ed5019a7eebf
PokaCris <after_the_rain999@mail.ru>
2025-11-19 21:25:49 +0300

    Add provider2

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,11 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use \Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function index(): JsonResponse 
     {
         return response()->json(User::all());
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|unique:user',
+            'email' => 'email|required|unique:user',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ], 422);
+        }
+
+        $validated = $validator->validated();
+        $hashedPassword = Hash::make($validated['password']);
+
+        $user = User::create([
+            'username' => $validated['username'],
+            'email' => $validated['email'],
+            'password_hash' => $hashedPassword
+        ]);
+        return response()->json($user, 201);
     }
 }
